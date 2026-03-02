e45c2a8c48d66d8003251b2cd31709151ea25fb0
alexthegoood <mail.alexthegoood@gmail.com>
2026-03-02 14:01:12 +0200

    update RegisterEvents.kt

--- core/src/main/kotlin/com/github/assemblyDir/paperKotlin/RegisterEvents.kt
+++ core/src/main/kotlin/com/github/assemblyDir/paperKotlin/RegisterEvents.kt
@@ -1,15 +1,29 @@
 package com.github.assemblyDir.paperKotlin
 
 import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.PluginManager
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
- * Registers one or more event listeners for this plugin.
+ * Registers one or more event listeners.
  *
+ * @receiver Your [JavaPlugin] instance
  * @param listeners the [Listener] instances to register
  */
 fun JavaPlugin.registerEvents(
     vararg listeners: Listener
+) = server.pluginManager.registerEvents(this, *listeners)
+
+/**
+ * Registers one or more event listeners.
+ *
+ * @receiver Your [PluginManager] instance
+ * @param listeners the [Listener] instances to register
+ */
+fun PluginManager.registerEvents(
+    plugin: Plugin,
+    vararg listeners: Listener
 ) = listeners.forEach { listener ->
-    server.pluginManager.registerEvents(listener, this)
+    registerEvents(listener, plugin)
 }
